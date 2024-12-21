09b4c58c8016ea76c1e20d5263c716e5944fb9c6
Fabrizio Fanini <fabrifanini@gmail.com>
2024-12-20 22:53:11 -0300

    Add a new function and update the constructor

--- room_rental.sol
+++ room_rental.sol
@@ -6,10 +6,11 @@ contract hotelRoom {
     struct Room {
         string name;
         string state;
+        uint price;
     }
 
     //events
-    event roomOccupied (string stateRoom);
+    event roomOccupied (string room, string stateRoom);
     event roomFreed(string roomName);
 
     //variables
@@ -19,23 +20,24 @@ contract hotelRoom {
     string[2] states = ["free", "occupied"];
     Room [] rooms;
     mapping (string => uint) indexRoom;
+    // string [5] rooms = ["Single room", "Double room", "Triple room", "Connecting room", "Suite"];
 
     //constructor
     constructor(){
         owner = payable(msg.sender);
-        rooms.push(Room ("single room", states[0]));
+        rooms.push(Room ("single room", states[0], price / 1 ether));
         indexRoom["Single room"] = 0;
 
-        rooms.push(Room ("double room", states[0]));
+        rooms.push(Room ("double room", states[0], price / 1 ether));
         indexRoom["Double room"] = 1;
 
-        rooms.push(Room ("triple room", states[0]));
+        rooms.push(Room ("triple room", states[0], price / 1 ether));
         indexRoom["triple room"] = 2;
 
-        rooms.push(Room ("connecting room", states[0]));
+        rooms.push(Room ("connecting room", states[0], price / 1 ether));
         indexRoom["connecting room"] = 3;
 
-        rooms.push(Room ("suite", states[0]));
+        rooms.push(Room ("suite", states[0], price / 1 ether));
         indexRoom["suite room"] = 4;
     }
 
@@ -54,26 +56,38 @@ contract hotelRoom {
     {
         uint i= indexRoom[_roomName];
         
-        require(msg.value >= price, "Insufficient funds");
-        require(compareStrings(_roomName, rooms[i].name), "The name doesn't exist.");
+        require(msg.value >= rooms[i].price, "Insufficient funds");
+        checkName(_roomName, i);
         require(compareStrings(rooms[i].state, states[0]), "Room is already occupied.");
 
         rooms[i].state = states[1];
 
         owner.transfer(msg.value);
 
-        emit roomOccupied(rooms[i].state);
+        emit roomOccupied(rooms[i].name, rooms[i].state);
     }
 
     function setRoomToFree (string memory _roomName) external onlyOwner
     {
         uint i = indexRoom[_roomName];
 
-        require(compareStrings(_roomName,rooms[i].name), "The name doesn't exist.");
-        
+        checkName(_roomName, i);
+
         rooms[i].state = states[0];
 
         emit roomFreed(_roomName);
+    }
+
+    function updatePrice (string memory _roomName, uint _price) external onlyOwner 
+    {
+        uint i = indexRoom[_roomName];
+        checkName(_roomName, i);
+        rooms[i].price = _price;
+    }
+
+    function checkName (string memory _roomName, uint i) internal view
+    {
+        require(compareStrings(_roomName,rooms[i].name), "The name doesn't exist.");
     }
 
     //modifier
